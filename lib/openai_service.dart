bca4ee5681f220a9dfc129417e748024aa5323eb
Shubhang <shubhangshukla001@gmail.com>
2024-02-04 23:57:53 +0530

    Gpt reply getting stored

--- lib/openai_service.dart
+++ lib/openai_service.dart
@@ -4,14 +4,15 @@ import 'package:chat_bot/secrets.dart';
 import 'package:http/http.dart' as http;
 
 class OpenAIService {
+  final List<Map<String, String>> messages = [];
+
   Future<String> isArtPromptAPI(String prompt) async {
     try {
-      
       final res = await http.post(
           Uri.parse('https://api.openai.com/v1/chat/completions'),
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer $openAPI1'
+            'Authorization': 'Bearer $openAPI'
           },
           body: jsonEncode({
             "model": "gpt-3.5-turbo",
@@ -23,14 +24,65 @@ class OpenAIService {
               }
             ]
           }));
-          print(res.body);
-          if(res.statusCode ==200)
-          {
-            print("Result recieved correctly");
-          }
-          return 'Ai';
+      print(res.body);
+      if (res.statusCode == 200) {
+        print("Result recieved correctly");
+        String content =
+            jsonDecode(res.body)['choices'][0]['message']['content'];
+        content = content.trim();
+        switch (content) {
+          case 'Yes':
+          case 'yes':
+          case 'Yes.':
+          case 'yes.':
+            final res = await dallEAPI(prompt);
+            return res;
+          default:
+            final res = await chatGPTAPI(prompt);
+            return res;
+        }
+      }
+      return 'An internal error occured';
     } catch (e) {
       return e.toString();
     }
+  }
+
+  Future<String> chatGPTAPI(String prompt) async {
+    messages.add({
+      'role': 'user',
+      'content': prompt,
+    });
+    try {
+      final res = await http.post(
+          Uri.parse('https://api.openai.com/v1/chat/completions'),
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $openAPI'
+          },
+          body: jsonEncode({
+            "model": "gpt-3.5-turbo",
+            "messages": messages,
+          }));
+      print(res.body);
+      if (res.statusCode == 200) {
+        print("Result recieved correctly");
+        String content =
+            jsonDecode(res.body)['choices'][0]['message']['content'];
+        content = content.trim();
+        
+        messages.add({
+          'role': 'assistant',
+          'content': content,
+        });
+      }
+      return 'An internal error occured';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  Future<String> dallEAPI(String prompt) async {
+    return 'ChatGPT';
   }
 }
